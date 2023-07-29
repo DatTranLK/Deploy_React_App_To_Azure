0c430f9f2316f4c2db3233e5d39a82c58f9a27be
Tran Thanh Dat <97006331+DatTranLK@users.noreply.github.com>
2023-07-29 14:11:43 +0700

    Dat: Pha time

--- src/views/pages/authentication/authentication/Login2.js
+++ src/views/pages/authentication/authentication/Login2.js
@@ -53,7 +53,10 @@ const Page = () => {
     });
 
     try {
-      fetch(`${apiUrl}/business-manager-authentication`, requestOptions)
+      fetch(
+        "http://parkzserver-001-site1.btempurl.com/api/business-manager-authentication",
+        requestOptions
+      )
         .then((response) => {
           return response.json();
         })
